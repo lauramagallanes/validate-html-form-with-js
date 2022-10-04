4125c3205be56db880fc0eb39a96a25328790bf9
lauramagallanes <pasocenturiontours@gmail.com>
2022-10-04 14:49:53 +0000

    descomente los mensajes de alerta

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ function envioFormulario(event) {
   let card = document.getElementById("card").value;
   let cardElement = document.getElementById("cardContainer");
   if (card == "") {
-    // alert(`el campo card esta vacio`);
+    alert(`el campo card esta vacio`);
     mensajeDeFalta.style.display = "block";
     cardElement.className = "alert alert-danger";
   }
@@ -22,7 +22,7 @@ function envioFormulario(event) {
   let cvc = document.getElementById("cvc").value;
   let cvcContainer = document.getElementById("cvcContainer");
   if (cvc == "") {
-    // alert(`el campo cvc esta vacio`);
+    alert(`el campo cvc esta vacio`);
     mensajeDeFalta.style.display = "block";
     cvcContainer.className = "alert alert-danger";
   }
@@ -30,7 +30,7 @@ function envioFormulario(event) {
   let amount = document.getElementById("amount").value;
   let amountContainer = document.getElementById("amountContainer");
   if (amount == "") {
-    // alert(`el campo amount esta vacio`);
+    alert(`el campo amount esta vacio`);
     mensajeDeFalta.style.display = "block";
     amountContainer.className = "alert alert-danger";
   }
@@ -38,7 +38,7 @@ function envioFormulario(event) {
   let nombre = document.getElementById("nombre").value;
   let nameContainer = document.getElementById("nameContainer");
   if (nombre == "") {
-    // alert(`el campo nombre esta vacio`);
+    alert(`el campo nombre esta vacio`);
     mensajeDeFalta.style.display = "block";
     nameContainer.className = "alert alert-danger";
   } else if (typeof nombre === "number") {
@@ -48,7 +48,7 @@ function envioFormulario(event) {
   let apellido = document.getElementById("apellido").value;
   let lastNameContainer = document.getElementById("lastNameContainer");
   if (apellido == "") {
-    // alert(`el campo apellido esta vacio`);
+    alert(`el campo apellido esta vacio`);
     mensajeDeFalta.style.display = "block";
     lastNameContainer.className = "alert alert-danger";
   }
@@ -56,7 +56,7 @@ function envioFormulario(event) {
   let city = document.getElementById("city").value;
   let cityContainer = document.getElementById("cityContainer");
   if (city == "") {
-    // alert(`el campo city esta vacio`);
+    alert(`el campo city esta vacio`);
     mensajeDeFalta.style.display = "block";
     cityContainer.className = "alert alert-danger";
   }
@@ -65,7 +65,7 @@ function envioFormulario(event) {
   let postalCodeContainer = document.getElementById("postalCodeContainer");
 
   if (postalCode == "") {
-    // alert(`el campo Postal Code esta vacio`);
+    alert(`el campo Postal Code esta vacio`);
     mensajeDeFalta.style.display = "block";
     postalCodeContainer.className = "alert alert-danger";
   }
@@ -73,7 +73,7 @@ function envioFormulario(event) {
   let mensaje = document.getElementById("mensaje").value;
   let messageContainer = document.getElementById("messageContainer");
   if (mensaje == "") {
-    // alert(`el campo Mensaje esta vacio`);
+    alert(`el campo Mensaje esta vacio`);
     mensajeDeFalta.style.display = "block";
     messageContainer.className = "alert alert-danger";
   }
@@ -85,7 +85,7 @@ function envioFormulario(event) {
   if (elementoActivo) {
     return true;
   } else {
-    // alert("No elegiste ninguna tarjeta");
+    alert("No elegiste ninguna tarjeta");
     mensajeDeFalta.style.display = "block";
     optionsContainer.className = "alert alert-danger";
   }
@@ -93,7 +93,7 @@ function envioFormulario(event) {
   let select = document.getElementById("inputState");
   let stateContainer = document.getElementById("stateContainer");
   if (select.value == "Choose...") {
-    // alert("No elegiste ningun state");
+    alert("No elegiste ningun state");
     mensajeDeFalta.style.display = "block";
     stateContainer.className = "alert alert-danger";
   }
